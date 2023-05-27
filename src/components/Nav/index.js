cf5a7526704a53174055331ef9fa797bbe9b267a
carnoldcoding <carnoldcoding@gmail.com>
2023-05-27 13:34:27 -0400

    navbar rework

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,45 +3,46 @@ import { useState } from "react";
 import "./NavStyles.scss";
 
 const Nav = () => {
-  const [activeIndex, setActiveIndex] = useState(-1);
-  const nav = [
-    {
-      link: "#Home",
-      icon: "home-outline",
-      text: "Home",
-    },
-    {
-      link: "#About",
-      icon: "person-circle-outline",
-      text: "About",
-    },
-    {
-      link: "#Portfolio",
-      icon: "library-outline",
-      text: "Portfolio",
-    },
-    {
-      link: "#Contact",
-      icon: "mail-outline",
-      text: "Contact",
-    },
-  ];
+  const [active, setActive] = useState("home");
   return (
     <nav>
       <ul>
-        {nav.map(({ link, icon, text }, index) => {
-          return (
-            <li
-              class={activeIndex === index && "active"}
-              onClick={() => {
-                setActiveIndex(index);
-              }}
-            >
-              <a href={link}>{text}</a>
-              <ion-icon name={icon}></ion-icon>
-            </li>
-          );
-        })}
+        <li
+          onClick={() => {
+            setActive("home");
+          }}
+          className={active === "home" && "active"}
+        >
+          <a href="#Home">Home</a>
+          <ion-icon name="home-outline"></ion-icon>
+        </li>
+        <li
+          onClick={() => {
+            setActive("about");
+          }}
+          className={active === "about" && "active"}
+        >
+          <a href="#About">About</a>
+          <ion-icon name="person-circle-outline"></ion-icon>
+        </li>
+        <li
+          onClick={() => {
+            setActive("portfolio");
+          }}
+          className={active === "portfolio" && "active"}
+        >
+          <a href="#Portfolio">Portfolio</a>
+          <ion-icon name="library-outline"></ion-icon>
+        </li>
+        <li
+          onClick={() => {
+            setActive("contact");
+          }}
+          className={active === "contact" && "active"}
+        >
+          <a href="#Contact">Contact</a>
+          <ion-icon name="mail-outline"></ion-icon>
+        </li>
         <aside>
           <div className="marker"></div>
         </aside>
