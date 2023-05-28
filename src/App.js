0acab0ef481716e4a4d1c0edf87054b7bfdffa70
carnoldcoding <carnoldcoding@gmail.com>
2023-05-27 22:17:27 -0400

    navbar responsive

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import Home from "./components/Home";
 import MobileNav from "./components/MobileNav";
 import "./App.scss";
 import ScrollSpy from "react-ui-scrollspy";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
   return (
     <>
-      <MobileNav />
+      {isMobile ? <MobileNav /> : <Nav />}
       <ContactBar />
       <ScrollSpy scrollThrottle={100} useBoxMethod={false}>
         <Home />
