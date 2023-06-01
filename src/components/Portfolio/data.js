ba8450438640d68bbdd5b800e47036a8e1b7c9f6
carnoldcoding <carnoldcoding@gmail.com>
2023-05-31 21:47:34 -0400

    stylistic changes

--- src/components/Portfolio/data.js
+++ src/components/Portfolio/data.js
@@ -9,21 +9,26 @@ const featured1 = {
 
 const projects = [
   {
+    title: "Sharaway Consulting",
+    description:
+      "This website serves as an advertisement for the services of an organization that helps non-profits get on their feet.",
+  },
+{
+    title: "Covid 19 and Politics",
+    description:
+      "This is a machine learning research paper written in Python using Jupytr Labs with the goal of representing a correlation between political affiliation and likelihood to contract COVID-19.",
+  },
+  {
     title: "NeoVim Configuration",
-    description: "This project contains the dotfiles and configuration tree for my personal development environment. The project is written entirely in Lua, and is formatted to allow quick installation on different devices.",
+    description:
+      "This project contains the dotfiles and configuration tree for my personal development environment. The project is written entirely in Lua, and is formatted to allow quick installation on different devices.",
   },
   {
     title: "iPhone Calculator",
-    description: "Remake of the iPhone calculator from scratch written in pure html/css/js.",
+    description:
+      "Remake of the iPhone calculator from scratch written in pure html/css/js.",
   },
-  {
-    title: "Covid 19 and Politics",
-    description: "This is a machine learning research paper written in Python using Jupytr Labs with the goal of representing a correlation between political affiliation and likelihood to contract COVID-19.",
-  },
-  {
-    title: "Sharaway Consulting",
-    description: "This website serves as an advertisement for the services of an organization that helps non-profits get on their feet.",
-  },
+  
 ];
 
 export { featured1, projects };
