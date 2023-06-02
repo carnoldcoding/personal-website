2f2e9d66fa36c83e149b9b2a46868b49cd66aa24
carnoldcoding <carnoldcoding@gmail.com>
2023-06-02 18:40:58 -0400

    about section animated

--- src/App.js
+++ src/App.js
@@ -9,48 +9,40 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import "./App.scss";
 import "./general.scss";
-import ScrollSpy from "react-ui-scrollspy";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-  const [Loading, setLoading] = useState(false);
-  const handleResize = () => {
-    setIsMobile(window.innerWidth <= 920);
-  };
-
-  useEffect(() => {
-      setTimeout(()=>{
-          setLoading(false);
-      }, 4000)
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+    const [Loading, setLoading] = useState(false);
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 920);
     };
-  });
-  return (
-    <>
-      {Loading ? (
-          <Loader />
-      ) : (
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 4000);
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    });
+    return (
         <>
-          {isMobile ? <MobileNav /> : <Nav />}
-          <ScrollSpy
-            activeClass="active"
-            offsetTop={10}
-            offsetBottom={100}
-            scrollThrottle={100}
-          >
-            <Home />
-            <About />
-            <Portfolio />
-            <Contact />
-          </ScrollSpy>
-          {isMobile ? <MobileFooter /> : <Footer />}
+            {Loading ? (
+                <Loader />
+            ) : (
+                <>
+                    {isMobile ? <MobileNav /> : <Nav />}
+                    <Home />
+                    <About />
+                    <Portfolio />
+                    <Contact />
+                    {isMobile ? <MobileFooter /> : <Footer />}
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 }
 
 export default App;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,27 +3,95 @@ import "./AboutStyles.scss";
 import Trait from "./Trait";
 import Experience from "./Experience";
 import { trait1, trait2, trait3, experience1 } from "./data";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
 
 const About = () => {
+  const scope = useRef();
+  const tl = useRef();
+  const fills = useRef([]);
+  const subtext = useRef();
+  const text = useRef();
+  const stack = useRef();
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      tl.current = gsap.timeline({
+        scrollTrigger: {
+          trigger: scope.current,
+          pin: true,
+          start: "top top",
+          end: "+=1000",
+          scrub: true,
+        },
+      });
+
+      fills.current.forEach((fill) => {
+        tl.current.to(
+          fill,
+          {
+            width: "100%",
+          },
+          "<"
+        );
+      });
+      tl.current.from(subtext.current, {
+        x: 100,
+        opacity: 0,
+      });
+      tl.current.from(text.current, {
+        x: -100,
+        opacity: 0,
+      });
+        tl.current.from(stack.current,{
+            opacity: 0
+        })
+    }, scope);
+    return () => ctx.revert();
+  }, []);
   return (
-    <section id="About">
+    <section id="About" ref={scope}>
       <article className="intro">
         <div className="title">
-          <h1 data-text="Cameron" style={{ "--clr": "#16BFE4" }}>
-            Cameron
-          </h1>
-          <h1 data-text="Arnold" style={{ "--clr": "white" }}>
-            Arnold
-          </h1>
-          <h3>I turn ideas into digital reality</h3>
+          <div>
+            <h1 className="outline" style={{ "--clr": "#16BFE4" }}>
+              Cameron
+            </h1>
+            <h1
+              ref={(e) => {
+                fills.current.push(e);
+              }}
+              className="fill"
+              style={{ "--clr": "#16BFE4" }}
+            >
+              Cameron
+            </h1>
+          </div>
+          <div>
+            <h1 className="outline" style={{ "--clr": "white" }}>
+              Arnold
+            </h1>
+            <h1
+              ref={(e) => {
+                fills.current.push(e);
+              }}
+              className="fill"
+              style={{ "--clr": "white" }}
+            >
+              Arnold
+            </h1>
+          </div>
+          <h3 ref={subtext}>I turn ideas into digital reality</h3>
         </div>
-        <p>
+        <p ref={text}>
           I'm a software engineer specializing in <i>building</i> and{" "}
           <i>designing</i> unique digital experiences. I prioritize readability
           and simplicity in my work, to promote understanding and communication
           with my peers.
         </p>
-        <aside>
+        <aside ref={stack}>
           <ion-icon name="logo-javascript"></ion-icon>
           <ion-icon name="logo-html5"></ion-icon>
           <ion-icon name="logo-css3"></ion-icon>
