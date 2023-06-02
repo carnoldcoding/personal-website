b7b3240e40b22c5cd3da18b141f05ae77acbc314
carnoldcoding <carnoldcoding@gmail.com>
2023-06-02 12:42:06 -0400

    loading screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Loader from "./components/Loader";
 import Nav from "./components/Nav";
-import Footer from "./components/Footer"; 
+import Footer from "./components/Footer";
 import MobileFooter from "./components/MobileFooter";
 import Home from "./components/Home";
 import MobileNav from "./components/MobileNav";
 import About from "./components/About";
-import Portfolio from "./components/Portfolio"
+import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import "./App.scss";
 import "./general.scss";
@@ -13,11 +14,15 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [Loading, setLoading] = useState(true);
   const handleResize = () => {
     setIsMobile(window.innerWidth <= 920);
   };
 
   useEffect(() => {
+      setTimeout(()=>{
+          setLoading(false);
+      }, 4000)
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
@@ -25,14 +30,25 @@ function App() {
   });
   return (
     <>
-      {isMobile ? <MobileNav /> : <Nav />}
-      <ScrollSpy activeClass="active" offsetTop={10} offsetBottom={100}  scrollThrottle={100}>
-        <Home />
-        <About />
-        <Portfolio />
-      <Contact />
-      </ScrollSpy>
-      {isMobile ? <MobileFooter /> : <Footer />}
+      {Loading ? (
+          <Loader />
+      ) : (
+        <>
+          {isMobile ? <MobileNav /> : <Nav />}
+          <ScrollSpy
+            activeClass="active"
+            offsetTop={10}
+            offsetBottom={100}
+            scrollThrottle={100}
+          >
+            <Home />
+            <About />
+            <Portfolio />
+            <Contact />
+          </ScrollSpy>
+          {isMobile ? <MobileFooter /> : <Footer />}
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/Loader/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoaderStyles.scss";
+
+const Loader = () => {
+  return (
+    <article className="loader">
+        <h1>C</h1>
+    </article>
+  );
+};
+
+export default Loader;
