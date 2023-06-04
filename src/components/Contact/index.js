26b31a593364676e93c7ebaa09a9220fef659ac2
carnoldcoding <carnoldcoding@gmail.com>
2023-06-04 14:46:12 -0400

    envelope added to contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,7 +5,27 @@ const Contact = () => {
   return (
     <section id="Contact">
       <h1>get in touch!</h1>
-      <p></p>
+      <div class="contact">
+        <div class="envelope">
+          <div class="top">
+            <div class="outer">
+              <div class="inner"></div>
+            </div>
+          </div>
+          <div class="bottom"></div>
+          <div class="left"></div>
+          <div class="right"></div>
+          <div class="cover"></div>
+          <div class="paper">
+            <a class="call" href="tel:9735088737">
+              973-508-8737
+            </a>
+            <a class="mail" href="mailto:carnoldcoding@gmail.com">
+              @carnoldcoding
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
