fc3b26f9a7fef341b9661a0d45d32a64615d4189
carnoldcoding <carnoldcoding@gmail.com>
2023-06-04 16:31:06 -0400

    contact section prototype

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,60 @@
 import React from "react";
 import "./ContactStyles.scss";
+import { useRef, useLayoutEffect } from "react";
+import gsap from "gsap";
 
 const Contact = () => {
+  const scope = useRef();
+  const tl = useRef();
+  const mail = useRef();
+  const letters = useRef([]);
+  const text = "contact";
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      tl.current = gsap.timeline({
+        scrollTrigger: {
+          trigger: scope.current,
+          start: "top center",
+        },
+      });
+      letters.current.forEach((letter) => {
+        tl.current.from(
+          letter,
+          {
+            y: -50,
+            opacity: 0
+          },
+          "-=.35"
+        ).to(letter,{
+            color: "white"
+        },"-=.45")
+      });
+
+      tl.current.from(mail.current, {
+        opacity: 0,
+        y: -100,
+      }, "-=.3");
+    }, scope);
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <section id="Contact">
-      <h1>get in touch!</h1>
-      <div class="contact">
+    <section id="Contact" ref={scope}>
+      <h1>
+        {text.split("").map((letter) => {
+          return (
+            <span
+              ref={(el) => {
+                letters.current.push(el);
+              }}
+            >
+              {letter}
+            </span>
+          );
+        })}
+      </h1>
+      <div class="contact" ref={mail}>
         <div class="envelope">
           <div class="top">
             <div class="outer">
